cd4be1b0a4c2502a44c860da66f8cf1cbe2943c9
tomiicab <pereyratomas1@hotmail.com>
2023-04-11 01:06:19 -0300

    agrego toolbar and rating

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ListadoPeliculasComponent } from './peliculas/listado-peliculas/listado-peliculas.component';
 import { ListadoGenericoComponent } from './utilidades/listado-generico/listado-generico.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module'
+import { MaterialModule } from './material/material.module';
+import { MenuComponent } from './menu/menu.component';
+import { RatingComponent } from './utilidades/rating/rating.component'
 @NgModule({
   declarations: [
     AppComponent,
     ListadoPeliculasComponent,
-    ListadoGenericoComponent
+    ListadoGenericoComponent,
+    MenuComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/utilidades/rating/rating.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent implements OnInit {
+
+  @Input()
+  maximoRating = 5;
+  @Input()
+  ratingSeleccionado = 0;
+  maximoRatingArr = [];
+  votado = false;
+  ratingAnterior;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.maximoRatingArr = Array(this.maximoRating).fill(0);
+  }
+
+  manejarMouseEnter(index:number):void{
+    this.ratingSeleccionado = index + 1;
+  }
+
+  manejarMouseLeave(){
+    if (this.ratingAnterior !== 0) {
+      this.ratingSeleccionado = this.ratingAnterior;
+    }else{
+      this.ratingSeleccionado = 0;
+    }    
+  }
+
+  rate(index : number):void{
+    this.votado = true;
+    this.ratingSeleccionado = index + 1;
+    this.ratingAnterior = this.ratingSeleccionado;
+  }
+}
